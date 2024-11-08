3826c9c6c6b302da2a5cc303c5d17db56933d3c0
Pranav Yerramaneni <69360966+DevMello@users.noreply.github.com>
2024-11-08 09:41:13 -0800

    Update script.js

--- slope/script.js
+++ slope/script.js
@@ -1,5 +1,5 @@
 let quickHideEnabled = true;
-let location = "https://pms-scusd-ca.schoolloop.com/portal/login";
+let locationChange = "https://google.com";
 document.addEventListener('keydown', function(event) {
 console.log(event)
 if((event.key == "q" || event.key == "Q") && event.altKey) {
@@ -7,7 +7,7 @@ if((event.key == "q" || event.key == "Q") && event.altKey) {
   quickHideEnabled = !quickHideEnabled
 } else if ((event.key == "q" || event.key == "Q") && quickHideEnabled == true) {
   document.write()
-  window.location = location
+  window.location = locationChange
 }
 })
 
